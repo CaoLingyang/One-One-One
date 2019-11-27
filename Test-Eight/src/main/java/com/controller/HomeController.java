610a94085b9488d229ecf89ea008246e02db2eb6
cao ling yang <2563875559@qq.com>
2019-11-27 11:56:40 +0800

    第二次提交

--- Test-Eight/src/main/java/com/controller/HomeController.java
+++ Test-Eight/src/main/java/com/controller/HomeController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
+
+
     @GetMapping("/")
     public String index(){
         return "index";
